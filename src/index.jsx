ae3ed0ebee87f9cb862d17ca758804bee4b46e3b
Iqbal Ahmed <admin@propattern.com>
2016-09-02 02:08:38 +0100

    Completion of client side code with fake data - pre-wire

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trianspotting', '28 days later'];
+const routes = <Route component={App}>
+    <Route path="/results" component={Results} />
+    <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-    <Voting pair={pair} hasVoted='Trainsptting'/>,
-    document.getElementById('app'));
+    <Router history={hashHistory}>{routes}</Router>,
+    document.getElementById('app')
+);
