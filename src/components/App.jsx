bf4266467201b0d6b26840f72f44d1da78959e84
Iqbal Ahmed <admin@propattern.com>
2016-09-02 19:05:45 +0100

    Minor fixes and removing overlooked fake data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default React.createClass({
     render: function() {
-        return React.cloneElement(this.props.children, {pair: pair, tally: tally});
+        return this.props.children;
     }
 });
